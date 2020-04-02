22437503de711dbace313473781bbec1cd0bbf12
Brittany Clark <clark.brittany.j@gmail.com>
2020-04-02 00:57:32 -0400

    updated seed file and handlebars connections

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Jalepeno Fire Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Jane (no Cheese)', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic', true);
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Patty');
